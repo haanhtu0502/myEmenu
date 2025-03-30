f790a4edf0ca5921beba64bde67c77546cc86405
tuha0502 <tuha@digitalbiz.com.vn>
2025-03-30 20:31:52 +0700

    add history list tab file

---
+++ lib/mvvm/view/cart/sub_tab/history_list_tab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HistoryListTab extends StatefulWidget {
+  const HistoryListTab({super.key});
+
+  @override
+  State<HistoryListTab> createState() => _HistoryListTabState();
+}
+
+class _HistoryListTabState extends State<HistoryListTab> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
